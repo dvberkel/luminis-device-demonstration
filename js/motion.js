4f652e42eaa1c9c2bb0fc38b98cf21d4e627a6c2
dvberkel <daan.v.berkel.1980+git@gmail.com>
2014-01-19 18:51:30 +0100

    refactor in preperation for total

--- js/motion.js
+++ js/motion.js
@@ -3,29 +3,24 @@ window.Motion = (function (Observable) {
     'use strict';
     var Motion = {};
 
-    var Data = Motion.Data = function () {
+    var Base = function () {
         Observable.call(this);
         this.id = '?';
-        this.x = 0;
-        this.y = 0;
-        this.z = 0;
-
     };
-    Data.prototype = new Observable();
-    Data.prototype.update = function (event) {
-        this.id = event.id;
-        this.x = event.x;
-        this.y = event.y;
-        this.z = event.z;
+    Base.prototype = new Observable();
+    Base.prototype.update = function (event) {
+        for (var key in event) {
+            this[key] = event[key];
+        }
         this.notify();
     };
 
-    var DataMap = Motion.DataMap = function () {
+    var BaseMap = function () {
         Observable.call(this);
         this.collection = {};
     };
-    DataMap.prototype = new Observable();
-    DataMap.prototype.update = function (id, event) {
+    BaseMap.prototype = new Observable();
+    BaseMap.prototype.update = function (id, event) {
         var isNew = false;
         if (! this.collection[id]) {
             this.collection[id] = new Data();
@@ -39,13 +34,65 @@ window.Motion = (function (Observable) {
         return this.collection[id];
     };
 
-    var standarFormatter = function (value) { return value; };
+    var BaseView = function (model, parent) {
+        this.model = model;
+        this.parent = parent;
+    };
+    BaseView.prototype.container = function (aClass) {
+        if (! this._container) {
+            var container = this._container = document.createElement('div');
+            if (aClass) {
+                container.setAttribute('class', aClass);
+            }
+            this.parent.appendChild(container);
+        }
+        return this._container;
+    };
+
+    var BaseMapView = Motion.DataMapView = function (model, parent, View) {
+        this.views = {};
+        this.model = model;
+        this.parent = parent;
+        this.View = View;
+    };
+    BaseMapView.prototype.register = function () {
+        this.model.addListener(this.update.bind(this));
+    };
+    BaseMapView.prototype.update = function () {
+        for (var id in this.model.collection) {
+            this.updateView(id);
+        }
+    };
+    BaseMapView.prototype.updateView = function (id) {
+        if (! this.views[id]) {
+            this.views[id] = new (this.View)(this.model.collection[id], this.container());
+        }
+    };
+    BaseMapView.prototype.container = function () {
+        return this.parent;
+    };
+
+    var Data = Motion.Data = function () {
+        Base.call(this);
+        this.x = 0;
+        this.y = 0;
+        this.z = 0;
+
+    };
+    Data.prototype = new Base();
+
+    var DataMap = Motion.DataMap = function () {
+        BaseMap.call(this);
+    };
+    DataMap.prototype = new BaseMap();
+
+    var standardFormatter = function (value) { return value; };
 
     var AttributeView = Motion.AttributeView = function (model, attribute, parent, formatter) {
         this.model = model;
         this.attribute = attribute;
         this.parent = parent;
-        this.formatter = formatter || standarFormatter;
+        this.formatter = formatter || standardFormatter;
         this.model.addListener(this.update.bind(this));
         this.update();
     };
@@ -63,49 +110,28 @@ window.Motion = (function (Observable) {
     };
 
     var DataView = Motion.DataView = function (model, parent) {
-        this.model = model;
-        this.parent = parent;
+        BaseView.call(this, model, parent);
         this.create();
     };
+    DataView.prototype = new BaseView();
     DataView.prototype.create = function () {
-        var container = this.container();
+        var container = this.container('motion');
         new AttributeView(this.model, 'id', container);
         new AttributeView(this.model, 'x', container, Motion.format.decimal(2));
         new AttributeView(this.model, 'y', container, Motion.format.decimal(2));
         new AttributeView(this.model, 'z', container, Motion.format.decimal(2));
     };
-    DataView.prototype.container = function () {
-        if (! this._container) {
-            var container = this._container = document.createElement('div');
-            container.setAttribute('class', 'motion');
-            this.parent.appendChild(container);
-        }
-        return this._container;
-    };
+
 
     var DataMapView = Motion.DataMapView = function (model, parent) {
-        this.views = {};
-        this.model = model;
-        this.parent = parent;
-        this.model.addListener(this.update.bind(this));
+        BaseMapView.call(this, model, parent, DataView);
+        this.register();
         this.update();
     };
-    DataMapView.prototype.update = function () {
-        for (var id in this.model.collection) {
-            this.updateView(id);
-        }
-    };
-    DataMapView.prototype.updateView = function (id) {
-        if (! this.views[id]) {
-            this.views[id] = new DataView(this.model.collection[id], this.container());
-        }
-    };
-    DataMapView.prototype.container = function () {
-        return this.parent;
-    };
+    DataMapView.prototype = new BaseMapView();
 
     Motion.format = {};
-    Motion.format.standard = standarFormatter;
+    Motion.format.standard = standardFormatter;
     Motion.format.decimal = function (decimals) {
         return function (value) {
             return Number(value).toFixed(decimals);
