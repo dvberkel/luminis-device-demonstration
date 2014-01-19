5bf18497590396d4cff53394dae2c6fb451882e3
dvberkel <daan.v.berkel.1980+git@gmail.com>
2014-01-19 15:21:15 +0100

    show motion data

--- js/presentation.js
+++ js/presentation.js
@@ -1,7 +1,17 @@
-/* global window:false, io:false */
-(function (io, undefined) {
+/* global window:false, document:false, Reveal:false, io:false, Motion:false, console:false */
+(function (Reveal, io, Motion, undefined) {
     'use strict';
+
+    var motionDataMap = new Motion.DataMap();
+    motionDataMap.update('a', { x: 1, y: 0, z: 0 });
+
+    var motionDataMapView;
+    Reveal.addEventListener('motion-events', function () {
+        if (!motionDataMapView) {
+            motionDataMapView = new Motion.DataMapView(motionDataMap, document.getElementById('motion-data'));
+        }
+    });
 
     var socket = io.connect(window.location.origin);
     socket.emit('presentation', {});
-})(io);
+})(Reveal, io, Motion);
--- server.js
+++ server.js
@@ -7,12 +7,14 @@ app.set('PORT', process.env.PORT || 1729);
 
 app.use('/static', express.static(__dirname + '/dist'));
 
+var presentations = {};
+
 io.sockets.on('connection', function(socket){
     console.log('socket %s connected', socket.id);
     socket.emit('id', { 'id': socket.id });
 
     socket.on('presentation', function(){
-	console.log('presentation received');
+	presentations[socket.id] = socket;
     });
 
     socket.on('log', function(data){
@@ -20,7 +22,11 @@ io.sockets.on('connection', function(socket){
     })
 
     socket.on('motion', function(event){
-	console.log(event);
+	console.log('event');
+	event.id = socket.id;
+	for (var id in presentations) {
+	    presentations[id].emit('motion', event);
+	}
     });
 });
 
